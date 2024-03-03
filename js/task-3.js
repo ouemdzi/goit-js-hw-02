7c8cce1e97de92823b98db2c1dac9b9e6fe3808b
ouemdzi <sashka.hf@gmail.com>
2024-03-03 16:56:43 +0100

    task 3 done

--- js/task-3.js
+++ js/task-3.js
@@ -29,3 +29,20 @@
 // Виклик функції checkForSpam("Trust me, this is not a spam message") повертає true
 // Виклик функції checkForSpam("Get rid of sPaM emails. Our book in on sale!") повертає true
 // Виклик функції checkForSpam("[SPAM] How to earn fast money?") повертає true
+
+function checkForSpam(message) {
+  if (message.toLowerCase().includes('spam')) {
+    return Boolean(true);
+  } else if (message.toLowerCase().includes('sale')) {
+    return Boolean(true);
+  } else {
+    return Boolean(false);
+  }
+}
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
